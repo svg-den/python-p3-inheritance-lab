c0d4b4e54f22ce416f6c782b72be2b8a2a78cd02
svg-den <sundaywanjiru@gmail.com>
2024-03-04 14:19:23 +0300

    correct

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,17 +6,6 @@ class Teacher(User):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
         self.knowledge = ["science", "maths", "social studies"]
-        # self.knowledge = [
-        #     "str is a data type in Python",
-        #     "programming is hard, but it's worth it",
-        #     "JavaScript async web request",
-        #     "Python function call definition",
-        #     "object-oriented teacher instance",
-        #     "programming computers hacking learning terminal",
-        #     "pipenv install pipenv shell",
-        #     "pytest -x flag to fail fast",
-        # ]
-
     def teach(self):
         return random.choice(self.knowledge)
 
